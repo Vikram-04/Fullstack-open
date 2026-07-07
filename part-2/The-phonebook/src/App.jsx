39015914a89a838b633ce15153f9d2afedefb2b7
Vikram-04 <vikramrawal7893@gmail.com>
2026-07-07 14:35:34 +0530

    Exercise 2.11 - Fetches initial data from server

--- part-2/The-phonebook/src/App.jsx
+++ part-2/The-phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Search = ({ search, handleSearch }) => {
   return (
@@ -49,12 +50,16 @@ const Numbers = ({ showPersons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();

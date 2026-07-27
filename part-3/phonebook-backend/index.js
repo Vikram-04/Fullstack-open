b879a5c5d24201441729e333dc8f395a780b025f
Vikram-04 <vikramrawal7893@gmail.com>
2026-07-27 20:28:23 +0530

    Till exercise 3.14

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,29 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const Person = require("./models/person");
 const app = express();
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.use(express.static("dist"));
 app.use(express.json());
@@ -37,34 +16,29 @@ app.use(
 );
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((people) => {
+    response.json(people);
+  });
 });
 
 app.get("/info", (request, response) => {
-  response.end(
-    `Phonebook has info of ${persons.length} people\n${new Date().toUTCString()}`,
-  );
+  Person.find({}).then((people) => {
+    response.end(
+      `Phonebook has info of ${people.length} people\n${new Date().toUTCString()}`,
+    );
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((p) => p.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).json({ error: `person with id ${id} does not exist` });
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((p) => p.id === id);
-  if (!person) {
-    response.status(404).json({ error: `Person with id ${id} does not exist` });
-    return;
-  }
-  persons = persons.filter((p) => p.id !== id);
-  response.json(person);
+  Person.findByIdAndDelete(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -75,22 +49,23 @@ app.post("/api/persons", (request, response) => {
       .json({ error: `Person name and number fields must exist` });
     return;
   }
-  if (persons.find((p) => p.name.toLowerCase() === body.name.toLowerCase())) {
-    response.status(400).json({
-      error: `Person with name ${body.name} already exists in phonebook`,
+  Person.find({ name: body.name }).then((people) => {
+    if (people.length != 0) {
+      return response.status(400).json({
+        error: `Person with name ${body.name} already exists in phonebook`,
+      });
+    }
+    const person = new Person({
+      name: body.name,
+      number: body.number,
     });
-    return;
-  }
-  const person = {
-    id: Math.floor(Math.random() * 999999999).toString(),
-    name: body.name,
-    number: body.number,
-  };
-  persons = persons.concat(person);
-  response.json(person);
+    person.save().then((savedPerson) => {
+      response.json(savedPerson);
+    });
+  });
 });
 
-PORT = process.env.PORT || 3001;
+PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
